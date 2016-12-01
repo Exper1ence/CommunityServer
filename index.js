44ad4574285843c6e3580fdcc09bc8ea0b5c90a9
Exper1ence <3533357503@qq.com>
2016-12-01 16:57:53 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('bluebird');
 const http = require('express')();
-const community = require('./bin/community');
-
+const path = require('path');
+const apps = require('dynamic-object-generator')(path.resolve(__dirname, 'bin'));
 http.set('port', (process.env.PORT || 5000));
 
-http.use(community);
+for (let app in apps) {
+    if (apps.hasOwnProperty(app)) {
+        http.use(apps[app]);
+    }
+}
 
 http.listen(http.get('port'), () => {
     console.log(`http server is running on port ${http.get('port')}.`);
